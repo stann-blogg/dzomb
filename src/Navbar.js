2a5ba3291bd77080d3d7d8fefc71925b6717695c
38810757 <99075650+38810757@users.noreply.github.com>
2023-01-23 21:45:31 +0300

    stanley1

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <nav className="Navbar">
+            <h1>The dojo blog</h1>
+            <div className="links">
+                <a href="google.com">Home</a><br></br>
+                <a href="/create">New blog</a>
+            </div>
+        </nav>
+
+    );
+}
+ 
+export default Navbar;
